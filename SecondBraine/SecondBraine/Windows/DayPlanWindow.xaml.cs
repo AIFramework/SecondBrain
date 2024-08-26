cd10c5e91af5f6dc0ab643f39ec64e1320228c7b
Zahar A. Ponimash <ponimashz@mail.ru>
2024-08-26 20:21:46 +0300

    Update DayPlanWindow.xaml.cs

--- SecondBraine/SecondBraine/Windows/DayPlanWindow.xaml.cs
+++ SecondBraine/SecondBraine/Windows/DayPlanWindow.xaml.cs
@@ -1,21 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace SecondBraine.Windows
 {
     /// <summary>
-    /// Представляет окно для отображения и редактирования плана на день.
+    /// Представляет окно для отображения и редактирования плана 
     /// </summary>
     public partial class DayPlanWindow : Window
     {
@@ -25,7 +13,7 @@ namespace SecondBraine.Windows
         
 
         /// <summary>
-        /// Создаёт новый экземпляр окна планирования на день.
+        /// Создаёт новый экземпляр окна планирования
         /// </summary>
         /// <param name="title">Заголовок окна.</param>
         public DayPlanWindow(PlanType type)
